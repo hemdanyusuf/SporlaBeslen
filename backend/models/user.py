387fc2dac97cde7124051251e2581fe36e7aabba
hemdanyusuf <hemdanyusuf@gmail.com>
2025-06-21 10:43:29 +0300

    WIP: Inventory modeli ekleniyor

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,10 @@
-class User:
-    def __init__(self, user_id, username):
-        self.id = user_id
-        self.username = username
+from sqlalchemy import Column, Integer, String
+from database.init import Base
+from sqlalchemy.schema import Table
+
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}  # 👈 Bu satır çözüm getirir
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
